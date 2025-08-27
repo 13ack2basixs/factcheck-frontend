d281640e523add96af5969a110e7ef51dfd331b6
Hong Wei Tai <e1138406@u.nus.edu>
2025-08-27 16:45:37 +0800

    style: add shadcn and revamp code styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,64 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+
+const FormSchema = z.object({
+  url: z.string(),
+})
+
 export default function Home() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      url: "",
+    },
+  })
+
+  // 3. Submit handler
+  function onSubmit(values: z.infer<typeof FormSchema>) {
+    console.log(values)
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 sm:p-10">
       <div className="gap-[10px] flex justify-end w-full">
-        <a href="#" className="px-4 py-2 rounded-md bg-[#5e4dcd] text-white cursor-pointer transition-colors duration-300 ease-in-out hover:bg-[#5e5dcd]">Login</a>
-        <a href="#" className="px-4 py-2 rounded-md bg-[#5e4dcd] text-white cursor-pointer transition-colors duration-300 ease-in-out hover:bg-[#5e5dcd]">Register</a>
+        <Button>Login</Button>
+        <Button>Register</Button>
       </div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <span className="text-[#5e4dcd] text-4xl font-bold">Paste a link. Uncover the truth.</span>
-        <form className="" action={"/"}>
-          <input 
-            type="text" 
-            name="url" 
-            placeholder="Paste a URL and verify its authenticity"
-            className="min-h-[50px] w-[300px] px-4 text-black text-[15px] border border-[#5e4dcd] rounded-l-md bg-transparent focus:border-[#3898EC] focus:outline-none" 
-          />
-          <input 
-            type="submit" 
-            value="Submit" 
-            className="min-h-[50px] px-4 py-2 rounded-r-md bg-[#5e4dcd] text-white text-[15px] cursor-pointer transition-colors duration-300 ease-in-out hover:bg-[#5e5dcd]" 
-          />
-        </form>
+        <span className="text-primary text-4xl font-bold">Paste a link. Uncover the truth.</span>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-row">
+            <FormField
+              control={form.control}
+              name="url"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      className="h-[40px] rounded-r-none w-[300px]" 
+                      placeholder="Paste a URL and verify its authenticity" {...field} 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="rounded-l-none">Submit</Button>
+          </form>
+        </Form>
       </main>
       <footer className="flex flex-col items-center justify-center">
         <span className="italic">Always FactCheck your sources!</span>
